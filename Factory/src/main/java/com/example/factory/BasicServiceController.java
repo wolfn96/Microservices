2eacec594d78e09417d0d7c8197a3034b07912d4
ViperPeak <nic.w.96@googlemail.com>
2021-02-15 15:19:49 +0100

    Der Zuul-Service wurde mit dem Discovery-Service verbunden.

--- Factory/src/main/java/com/example/factory/BasicServiceController.java
+++ Factory/src/main/java/com/example/factory/BasicServiceController.java
@@ -1,14 +1,15 @@
 package com.example.factory;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.AutoConfigureOrder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class BasicServiceController {
-
     BasicService service;
+
 
     @GetMapping(path="/greeting")
     public String greeting(){
