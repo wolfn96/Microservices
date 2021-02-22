629d5bf2fa9fff12ae6517bf5964442c7cbedd10
ViperPeak <nic.w.96@googlemail.com>
2021-02-22 13:32:12 +0100

    Es wurde eine eindeutige Antwort für einen Service konfiguriert. Außerdem wurde eine Shutdown-Methode implementiert, mit der ein Service heruntergefahren werden kann.

--- Service/src/main/java/com/example/service/controller/ServiceController.java
+++ Service/src/main/java/com/example/service/controller/ServiceController.java
@@ -6,18 +6,27 @@ import com.example.service.view.ServiceView;
 import com.fasterxml.jackson.databind.JsonNode;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.WebApplicationType;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.ws.rs.core.Application;
+
 @RestController
 public class ServiceController {
 
 @Value("${random.value}")
 private String randomValue;
+
+@Autowired
+private ApplicationContext appContext;
 
 @Autowired
     ServiceView<JsonNode> view;
@@ -27,13 +36,18 @@ private String randomValue;
     return view.greeting(name)+" from Server with random Value "+randomValue;
 }
 
+@GetMapping(path="/shutdown")
+public void shutdown(){
+ SpringApplication.exit(appContext, () -> 0);
+}
+
 @PostMapping(path = "/greetingJson",
             consumes = MediaType.APPLICATION_JSON_VALUE,
             produces = MediaType.APPLICATION_JSON_VALUE)
     public String greetingJson(@RequestParam String contentJson) {
 
         JsonNode node = view.greetingGeneric(contentJson);
-        return node.toPrettyString();
+        return node.toPrettyString()+" "+randomValue;
 
     }
 
